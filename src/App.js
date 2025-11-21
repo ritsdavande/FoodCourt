aa39898c15df4466c222e1bebff3d5fb5c6b67dc
Prathamesh Davande <prathamesh.pd.2000@gmail.com>
2025-11-22 00:25:55 +0530

    Initial commit: MERN food ordering app with Vercel deployment config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./components/ContextReducer";
+
+// Import screens/pages (these should include Navbar at their top)
+import Home from "./screens/Home";
+import Cart from "./screens/Cart";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
+import MyOrder from "./screens/MyOrder";
+import ForgotPassword from "./screens/ForgotPassword";
+// Add additional screens if needed
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/myorders" element={<MyOrder />} />
+          <Route path="/resetpassword" element={<ForgotPassword />} />
+          {/* Add other routes as needed */}
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
